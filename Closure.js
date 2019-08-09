09b6a110aef1c187c6ac9716bdc2c30c89da7592
Martín Cerdeira <martincerdeira@gmail.com>
2019-08-09 11:53:11 -0300

    Update Closure.js

--- Closure.js
+++ Closure.js
@@ -1,1 +1,13 @@
-//
+function MakeCounter(count){  
+	return function(){  	 
+     return count++;
+  }
+}
+
+var counter = MakeCounter(5);
+
+console.log(counter()); // 5
+console.log(counter()); // 6
+console.log(counter()); // 7
+console.log(counter()); // 8
+console.log(counter()); // 9
